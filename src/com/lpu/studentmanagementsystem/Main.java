a8d8e990704b555f3d2acd02106a1308461efc2f
Srideepthi2384 <srideepthiedupuganti@gmail.com>
2023-03-13 00:22:48 +0530

    initial commit sstudent-management-system

---
+++ src/com/lpu/studentmanagementsystem/Main.java
@@ -0,0 +1,9 @@
+package com.lpu.studentmanagementsystem;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
---
+++ src/com/lpu/studentmanagementsystem/component/RegistrationComponent.java
@@ -0,0 +1,5 @@
+package com.lpu.studentmanagementsystem.component;
+
+public class RegistrationComponent {
+
+}
